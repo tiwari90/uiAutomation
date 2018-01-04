cd52faf9be1fb893d5271136a33f96a5a10cfd30
ashishti <ashishti@192.168.159.1>
2018-01-04 18:58:43 +0530

    Added New User Creation Page. Added My Account Page. Updated Home page methods and Webelements.

--- src/main/java/com/test/automation/uiAutomation/uiActions/HomePage.java
+++ src/main/java/com/test/automation/uiAutomation/uiActions/HomePage.java
@@ -1,72 +1,25 @@
 package com.test.automation.uiAutomation.uiActions;
 
-import java.util.concurrent.TimeUnit;
-
 import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
 
 	public static final Logger logger = Logger.getLogger(HomePage.class.getName());
 	WebDriver driver;
-	WebDriverWait wait;
 	
 	@FindBy(xpath = "//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a")
 	WebElement signInBtn;
-	
-	@FindBy(xpath = "//*[@id=\"email\"]")
-	WebElement loginEmailTxtBox;
-	
-	@FindBy(xpath = "//*[@id=\"passwd\"]")
-	WebElement loginPwdTxtBox;
-	
-	@FindBy(xpath = "//*[@id=\"SubmitLogin\"]")
-	WebElement submitBtn;
-	
-	@FindBy(xpath = "//*[@id=\"center_column\"]/div[1]/ol/li")
-	WebElement authenticationFailedMsg;
-	
-	@FindBy(xpath = "//*[@id=\"email_create\"]")
-	WebElement newEmailAddressTxtBox;
-	
-	@FindBy(xpath = "//*[@id=\"SubmitCreate\"]/span")
-	WebElement createAnAccountBtn;
-	
 	
 	public HomePage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
 	}
 	
-	public void loginToApplication(String email, String password) {
-		clickSignIn();
-		logger.info("Clicked on sign in button");
-		loginEmailTxtBox.sendKeys(email);
-		logger.info("Entered login email address");
-		loginPwdTxtBox.sendKeys(password);
-		logger.info("Entered login password");
-		submitBtn.click();
-		logger.info("Clicked on submit button");
-	}
-	
-	public String getAuthenticationFailedMsg() {
-		logger.info("Fetching authentication failed error message");
-		return authenticationFailedMsg.getText();
-	}
-	
 	public void clickSignIn() {
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		signInBtn.click();
-	}
-	
-	public void createAccount(String emailAddress) {
-		clickSignIn();
-		wait.until(ExpectedConditions.elementToBeClickable(createAnAccountBtn));
-		newEmailAddressTxtBox.sendKeys(emailAddress);
-		createAnAccountBtn.click();
+		logger.info("Clicked on sign in button");
 	}
 }
--- src/test/java/com/test/automation/uiAutomation/homepage/TC001_VerifyLoginWithInvalidCredentials.java
+++ src/test/java/com/test/automation/uiAutomation/homepage/TC001_VerifyLoginWithInvalidCredentials.java
@@ -8,24 +8,27 @@ import org.testng.annotations.Test;
 
 import com.test.automation.uiAutomation.testBase.TestBase;
 import com.test.automation.uiAutomation.uiActions.HomePage;
+import com.test.automation.uiAutomation.uiActions.MyAccountPage;
 
 public class TC001_VerifyLoginWithInvalidCredentials extends TestBase{
  
 	public static final Logger logger = Logger.getLogger(TC001_VerifyLoginWithInvalidCredentials.class.getName());
-	HomePage homePage;
+	MyAccountPage myAccount;
+	HomePage homepage;
 	
 	@BeforeTest
 	public void setup() {
 		init();
 	}
 	
-	
 	@Test
 	public void verifyLoginWithInvalidCredentials() {
 		logger.info("******************* Verification Started *******************");
-		homePage = new HomePage(driver);
-		homePage.loginToApplication("email", "password");
-		Assert.assertEquals(homePage.getAuthenticationFailedMsg(), "Invalid email address.");
+		homepage = new HomePage(driver);
+		homepage.clickSignIn();
+		myAccount = new MyAccountPage(driver);
+		myAccount.loginToApplication("email", "password");
+		Assert.assertEquals(myAccount.getAuthenticationFailedMsg(), "Invalid email address.");
 		logger.info("******************* Verification Completed *******************");
 	}
 	
